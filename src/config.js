8da9adab6078d2931ff3130f75f7068fa2a6808f
7henoble1 <61429724+7henoble1@users.noreply.github.com>
2020-04-02 12:25:34 -0700

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    STRIPE_KEY: "Ypk_test_0QHHK5qWl2fdobnqB7nTBuFK00W8nikOAk",
     s3: {
      REGION: "us-east-1",
-     BUCKET: "elbert-ballo-notes-app-uploads"
+     BUCKET: "my-notes-app"
     },
     apiGateway: {
      REGION: "us-east-1",
-     URL: "https://hbzgm5jveh.execute-api.us-east-1.amazonaws.com/prod"
+     URL: "https://vlg7n20px5.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
      REGION: "us-east-1",
-     USER_POOL_ID: "us-east-1_7vp3PZ8LD",
-     APP_CLIENT_ID: "210575m5oo96c6d9d7h819c0no",
-     IDENTITY_POOL_ID: "us-east-1:8e769d38-64dc-4188-9523-34f91137a473"
+     USER_POOL_ID: "us-east-1_4xc1y4oSn",
+     APP_CLIENT_ID: "11dkkrkdn95mn45u278bkp3h94",
+     IDENTITY_POOL_ID: "us-east-1:5eb2ddbe-e4c8-4f5e-a89e-7ccd203ccb77"
     }
    };
